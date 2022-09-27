3e1f2730a29ac4d49728c6d31ee23f726b44649a
Avery2 <justinaverychan@gmail.com>
2022-09-26 17:33:50 -0700

    feat: generate slack string of ducks

---
+++ party_duck_generator.py
@@ -0,0 +1,16 @@
+import random
+
+ducks_and_weight = {
+    "partyduck": 1000,
+    "partyduck_dance1": 1000,
+    "partyduck_dance2": 1000,
+    "partyduccc": 200,
+    "party-dinosaur": 1,
+}
+
+num_ducks = 100
+
+ducks = [f":{d}:" for d in random.choices(list(ducks_and_weight.keys()), list(ducks_and_weight.values()), k=num_ducks)]
+
+slack_string=' '.join(ducks)
+print(f"{slack_string}")
